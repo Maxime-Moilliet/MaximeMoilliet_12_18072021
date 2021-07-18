6d8973d2c8453e136a4f3193cb1f3a274f57c2ad
Maxime-Moilliet <mlltmax66@gmail.com>
2021-07-18 18:34:09 +0200

    modify(Dashboard) create grid

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -6,6 +6,21 @@ export class Dashboard extends React.Component {
         return (
             <main className="container__main">
                 <UserInfo />
+                <div className="dashboard">
+                    <div className="dashboard__container">
+                        <div className="dashboard__container-left">
+                            {/* <BarChartActivities /> */}
+                            <div className="dashboard__container-left-bottom">
+                                {/* <LineChartAverageSession /> */}
+                                {/* <RadarChartScore /> */}
+                                {/* <PieChartScore /> */}
+                            </div>
+                        </div>
+                        <div className="dashboard__container-right">
+                            {/* <KeyData /> */}
+                        </div>
+                    </div>
+                </div>
             </main>
         )
     }
